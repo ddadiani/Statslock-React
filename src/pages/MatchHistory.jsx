483dfe48d328d934fb900efc16346f8f58858e6e
dadian1 <daviddadiann@gmail.com>
2026-07-03 20:12:23 +0400

    add localstorage function for match history. saves last valid accountid

--- src/pages/MatchHistory.jsx
+++ src/pages/MatchHistory.jsx
@@ -10,6 +10,9 @@ function MatchHistory() {
   const [matches, setMatches] = useState([]);
   const [error, setError] = useState(null);
   const [heroMap, setHeroMap] = useState([]);
+  const [accountId, setAccountId] = useState(() => {
+    return localStorage.getItem("steam-account-id") || "";
+  });
 
   // Get all heroes and keep them in a map
   // Needed to match every different hero per match in match history
@@ -23,19 +26,43 @@ function MatchHistory() {
     })
   }, [])
 
+  useEffect(() => {
+    if (accountId) {
+      handleSearch(accountId);
+    }
+  }, [])
+
+  // Simple regex to check valid Account ID
+  const isValidInt = (str) => /^\d+$/.test(str.trim());
 
   function handleKeyDown(e) {
     if (e.key === 'Enter') handleSearch();
   }
 
-  async function handleSearch() {
-    if (!query.trim()) return // Don't do anything if search is empty
+  async function handleSearch(id = query) {
+    const trimmed = id.toString().trim();
+    // Don't do anything if search is empty
+    if (!trimmed) return
+    // Check if it's a valid ID
+    else if (!isValidInt(trimmed)) {
+      setError(new Error(`Player with AccountID ${trimmed} doesn't exist`));
+      setMatches([]);
+      return;
+    }
+
     setError(null);
     setMatches([]);
 
     try {
-      const data = await getPlayerHistory(query.trim());
-      setMatches(data.slice(0, 50));
+      const data = await getPlayerHistory(trimmed);
+      // If data is empty, which happens when Steam user hasn't played any Deadlock matches
+      if (data.length === 0) {
+        setError(new Error("No history found"));
+        return;
+      }
+      setMatches(data.slice(0, 50)); // Display matches (50)
+      setAccountId(trimmed); // Set account ID
+      localStorage.setItem("steam-account-id", trimmed); // Save account ID to local storage
     } catch (err) {
       setError(err);
     }
@@ -53,9 +80,11 @@ function MatchHistory() {
         hasError={!!error}
       />
       {error && <p>{error.message}</p>}
-      <div id="history">
-        {matches && matches.length > 0 && (
-          <>
+
+      {matches && matches.length > 0 && (
+        <>
+          <p className="hint" style={{ margin: 0 }}>AccountId: {matches[0].account_id}</p>
+          <div id="history">
             {matches.map((match) => (
               <MatchCard
                 key={match.match_id}
@@ -64,10 +93,9 @@ function MatchHistory() {
               />
             ))}
             <p className="hint">Last 50 matches loaded</p>
-          </>
-        )}
-      </div>
-
+          </div>
+        </>
+          )}
       <AccountIdSuggestion />
     </>
   )
